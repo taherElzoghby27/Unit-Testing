807377172a96f94a543cd346bec5f338c4f8e315
taherElzoghby27 <tataamen678@gmail.com>
2025-09-16 02:11:04 +0300

    added suite class

--- src/test/java/com/spring/boot/unittesting/auth/SuiteTest.java
+++ src/test/java/com/spring/boot/unittesting/auth/SuiteTest.java
@@ -1,4 +1,12 @@
 package com.spring.boot.unittesting.auth;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        UserServiceTest.class,
+        LoginServiceTest.class,
+})
 public class SuiteTest {
 }
