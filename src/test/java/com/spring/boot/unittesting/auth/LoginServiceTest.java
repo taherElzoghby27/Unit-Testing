d5bc3c02964bdb158db3de8fdbb35d1038763604
taherElzoghby27 <tataamen678@gmail.com>
2025-09-16 01:11:25 +0300

    added login service test

---
+++ src/test/java/com/spring/boot/unittesting/auth/LoginServiceTest.java
@@ -0,0 +1,23 @@
+package com.spring.boot.unittesting.auth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginServiceTest {
+
+    @Test
+    public void authenticateSuccess() {
+        LoginService loginService = new LoginService();
+        boolean result = loginService.authenticate("admin", "admin");
+        assertTrue("user must authenticated", result);
+    }
+
+    @Test
+    public void authenticateFailure() {
+        LoginService loginService = new LoginService();
+        boolean result = loginService.authenticate("admin1", "admin");
+        assertFalse("user shouldn't authenticate", result);
+    }
+}
